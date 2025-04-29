81a7c5285a604d90687b1ac89372eb25277d5fd8
shreyansh-web <shreyansh487@gmail.com>
2025-04-29 23:40:38 +0530

    fix

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,38 +1,33 @@
-"use client"
+"use client";
 
-import React, { useCallback, useEffect } from "react"
+import React, { useCallback, useEffect } from "react";
 import { io } from "socket.io-client";
 
-interface SocketProviderProps{
-    children?: React.ReactNode
+interface SocketProviderProps {
+  children?: React.ReactNode;
 }
 
-interface ISocketContext{
-    sendMessage: (msg : string) => any;
+interface ISocketContext {
+  sendMessage: (msg: string) => any;
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
 
+export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  useEffect(() => {
+    const _socket = io("http://localhost:8002");
 
+    return () => {
+      _socket.disconnect();
+    };
+  }, []);
 
-export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
-
-    useEffect(() => {
-        const _socket = io('http://localhost:8002');
-
-        return () => {
-            _socket.disconnect();
-        }
-
-
-    },[])
-
-    const sendMessage:ISocketContext['sendMessage'] = useCallback((msg) => {
-        console.log("Send Message", msg);
-    },[])
-    return(
-        <SocketContext.Provider value={{sendMessage}}>
-            {children}
-            </SocketContext.Provider>
-    )
-}
+  const sendMessage: ISocketContext["sendMessage"] = useCallback((msg) => {
+    console.log("Send Message", msg);
+  }, []);
+  return (
+    <SocketContext.Provider value={{ sendMessage }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
